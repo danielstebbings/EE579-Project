5e8132095a8e989224f9ca2f6ecc706a33951e48
Daniel Stebbings <danielastebbings@gmail.com>
2025-02-17 01:26:30 +0000

    ai dev

---
+++ AI_Development/README.md
@@ -0,0 +1,3 @@
+# AI development
+Note: When using setup_datasets.py, need to edit data.yaml to be an absolute path
+~ does not get evaluated.
--- AI_Development/train.py
+++ AI_Development/train.py
@@ -13,7 +13,7 @@ from ultralytics import YOLO
 parser = argparse.ArgumentParser(description="Train a YOLO model on a dataset")
 parser.add_argument("--data",   type=str, help="Path to the data.yaml file",    default="/home/daniel/github/EE579-Project/AI_Development/datasets/cansuperset-1/data.yaml")
 parser.add_argument("--model",  type=str, help="Model name",                    default="yolo11n.pt")
-parser.add_argument("--epochs", type=int, help="Number of epochs to train",     default=1)
+parser.add_argument("--epochs", type=int, help="Number of epochs to train",     default=32)
 parser.add_argument("--out",    type=str, help="Output directory for model",    default="./model_runs")
 args = parser.parse_args()
 
@@ -27,6 +27,9 @@ fitness = val_result.fitness  # Show the validation results
 # create model name based on map50, epochs, date, and time
 model_name = f"yolo11n_{fitness:.2f}_{args.epochs}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}"
 # Save the model with the new name
+if not os.path.exists(args.out):
+    os.makedirs(args.out)
+
 model.save(f"{args.out}/{model_name}.pt")
 
 val_result.mean_results()
